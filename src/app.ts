d2d92bd65116312151fea7144b7945a237b602db
mosakung <49138554+mosakung@users.noreply.github.com>
2023-01-08 01:09:02 +0700

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,16 @@
-import DiscordActivity from "./discord-init/DiscordActivity"
+const integer: number = 1
+const char: String = 'a'
+const float: number = 3.14
+const str: String = "mos"
 
-DiscordActivity.login()
+var a: String = 'a'
 
+console.log(`Hello World ${a} 123`)
+
+a = 'b'
+
+console.log(`Hello World ${a} 123`)
+
+a = a + 'b'
+
+console.log(`Hello World ${a} 123`)
